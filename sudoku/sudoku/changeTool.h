c6ef5439b109ff52ddfb44a59de989ccfeea630c
thousfeet <thousfeet@qq.com>
2017-09-08 16:43:19 +0800

    初版完成

---
+++ sudoku/sudoku/changeTool.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef CHANGETOOL_H
+#define CHANGETOOL_H
+
+/*
+类名：changeTool
+功能：提供矩阵变换和处理的工具函数，其中包括：
+	  交换数字；交换行、列；交换大行、大列；
+	  修正第一项元素值；输出矩阵
+*/
+
+class changeTool
+{
+public:
+	changeTool(int mat[][9]);                 //赋值二维数组Mat为初始矩阵
+	void swapNumber(int num1,int num2);       //交换数字（参数范围1~9）
+	void swapRow(int i, int row1, int row2);  //交换行
+	void swapCol(int i, int col1, int col2);  //交换列
+	void swap3Row(int squ1, int squ2);        //以三行交换宫（参数范围0~2）
+	void swap3Col(int squ1, int squ2);        //以三列交换宫（参数范围0~2）
+	void check();                             //检查第一项是否为5，若非则修正
+	void printMat();                          //输出
+private:
+	int Mat[9][9];
+};
+
+#endif //!CHANGETOOL_H
---
+++ sudoku/sudoku/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
+#pragma once
+
+#include "targetver.h"
+#include "changeTool.h"
+#include "sudokuMat.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include<cstdlib>
+#include<cstring>
+#include<cctype>
+#include<iostream>
+#include <time.h>
+#include <fstream>
+
+// TODO: 在此处引用程序需要的其他头文件
